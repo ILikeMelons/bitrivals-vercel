0a4c28d5aadfd5a99dc004da6b8441e3091b2117
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2021-11-29 18:17:45 -0500

    script id

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,7 @@ function MyApp({ Component, pageProps }) {
       src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
       />
       <Script 
+      id='datalayerScript'
       strategy='lazyOnload'
       >
         {
