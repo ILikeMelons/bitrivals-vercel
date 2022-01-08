8e7ab76c8d4245074807db82bf09b2cd21bf72b1
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2022-01-07 20:43:06 -0500

    added userContext to token page

--- pages/token.js
+++ pages/token.js
@@ -7,20 +7,18 @@ import Roadmap from '../sections/Roadmap'
 import Team from '../sections/Squad'
 import Footer from '../sections/Footer'
 import Tokenomics from '../sections/Tokenomics/tokenomics'
-
+import { UserContextProvider } from '../hooks/authUser'
 export default function Home() {
-  const [user, setUser] = useState(null);
-  useEffect(()=>{
-    setUser(supabase.auth.user());
-  }, [])
   return (
       <main className='relative overflow-hidden bg-black-50'>
-        <Hero user={user} variant={'token'} />
+        <UserContextProvider>
+        <Hero variant={'token'} />
         <Tokenomics />
         <Why />
         <Roadmap />
         <Team />
         <Footer />
+        </UserContextProvider>
       </main>
  
   )
