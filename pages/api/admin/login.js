536cd88b830b12642cf3e1af338af708e4ef642e
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2022-02-08 14:24:58 -0500

    update admin login api

--- pages/api/admin/login.js
+++ pages/api/admin/login.js
@@ -3,8 +3,8 @@ export default async function loginUser(req, res) {
     // destructure the e-mail and password received in the request body.
     const { password } = req.body
    
-    //console.log(email);
-    if(password == '6sM2rVp&rcRw@Fos'){
+    
+    if(password == process.env.ADMIN_DASHBOARD_PSW){
         return res.status(200).json({success : true});
     }
    
