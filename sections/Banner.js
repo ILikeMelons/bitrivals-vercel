9de7aa655749ee1ec433ef5a2e052dbea24b3f5b
Fox <>
2022-02-08 16:19:59 +0000

    Fixed medium icon. Tweaked the banner logo order and sizing / colour

--- sections/Banner.js
+++ sections/Banner.js
@@ -1,31 +1,30 @@
-import { coingecko, nomics, coinmarketcap, certik, yahoo, marketwatch, nanuque } from "../public";
+import { certik, yahoo, marketwatch, nanuque } from "../public";
 import Container from "../components/Container";
 import Link from 'next/link'
 
 const icons = [
     {
+        image : certik.src,
+        height : '40px',
+        className: 'mt-3 sm:pr-4',
+        link: 'https://www.certik.org/projects/bit-rivals'
+    },
+    {
         image : nanuque.src,
-        height: '50px',
+        height: '52px',
         className: 'mt-3',
         link: 'https://nanuqe.com/bit-rivals/'
     },
     {
-        image : certik.src,
-        height : '30px',
-        className: 'mt-3 sm:pr-4',
-        link: 'https://www.certik.org/projects/bit-rivals'
-    },
-    
-    {
         
         image : yahoo.src,
-        height : '30px',
+        height : '28px',
         className: 'mt-4 sm:pr-4',
         link: 'https://finance.yahoo.com/news/bit-rivals-rewards-playing-favorite-140000474.html'
     },
     {
         image : marketwatch.src,
-        height : '28px',
+        height : '26px',
         className: 'mt-3',
         link: 'https://www.marketwatch.com/press-release/bit-rivals-rewards-you-for-playing-your-favorite-games-presale-starts-january-21-2022-01-10?tesla=y'
     }
