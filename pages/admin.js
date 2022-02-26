6f4e2c97298f20aceec56a1f46620edeea8ee197
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2022-02-26 13:53:17 -0500

    commented import

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,4 @@
-import Table from '../components/Table';
+//import Table from '../components/Table';
 import {getAllUsers} from '../backend/Supabase' 
 import { useState } from 'react';
 import { login } from '../requests/admin';
