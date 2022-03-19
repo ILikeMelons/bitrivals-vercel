fd9d96d9f1e516564bc2f4cce8b49ef7c63c1547
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2022-03-18 23:55:02 -0400

    Changed allocation section + new litepaper

--- sections/Allocations/allocations.js
+++ sections/Allocations/allocations.js
@@ -8,6 +8,7 @@ import { circle_solid_white, cross_solid_yellow, square_outline_yellow, triangle
 import handleViewport from "react-in-viewport";
 import style from './style.module.css'
 
+
 const titleBlock = (props) => {
 	const {inViewport, forwardedRef} = props;
 	const opacity = inViewport ? 0.2 : 0;
@@ -33,45 +34,53 @@ const Allocations = (props) => {
 				
 				<ViewportBlock />
 
-				<div className="relative flex flex-col-reverse justify-between w-full mt-12 md:flex-row gap-x-12 gap-y-12 md:gap-y-0 md:mt-0">
+				<div className="relative flex flex-col-reverse justify-between w-full mt-12 md:flex-row gap-x-8 gap-y-12 md:gap-y-0 md:mt-0">
 					<div className="flex items-center">
 						<div className="grid w-full h-auto grid-cols-2 gap-y-8 gap-x-12">
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#FB385B'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">40%</span></div>
-								<div className="text-white text-14px">Staking/LP Rewards</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#1e2161', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#3935eb'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">25%</span></div>
+								<div className="text-white text-14px">Rewards</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#C838FB'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">5%</span></div>
-								<div className="text-white text-14px">Marketing</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#6c5730', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#f1b43e'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">12%</span></div>
+								<div className="text-white text-14px">Private sale</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#7738FB'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">6%</span></div>
-								<div className="text-white text-14px">Team allocation</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#713f28', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#e36d2c'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">10%</span></div>
+								<div className="text-white text-14px">Public sale</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#3C38FB'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">7%</span></div>
-								<div className="text-white text-14px">Rival Reserve</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#466024', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#a9e132'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">6%</span></div>
+								<div className="text-white text-14px">PCS liquidity</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#38B5FB'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">4%</span></div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#2e2161', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#7236f0'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">5%</span></div>
+								<div className="text-white text-14px">Advisors / Partners</div>
+							</div>
+							<div className="flex items-center">
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#3e2058', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#bb37ed'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">6%</span></div>
+								<div className="text-white text-14px">Team allocations</div>
+							</div>
+							<div className="flex items-center">
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#204e6d', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#34a5e6'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">5%</span></div>
 								<div className="text-white text-14px">Development</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#00D897'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">3%</span></div>
-								<div className="text-white text-14px">Investors</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#0b6352', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#04b382'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">5%</span></div>
+								<div className="text-white text-14px">Marketing</div>
 							</div>
 							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#F9BA3F'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">25%</span></div>
-								<div className="text-white text-14px">Presale</div>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#5d1e49', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#e82dca'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">3%</span></div>
+								<div className="text-white text-14px">CEX liquidity</div>
 							</div>
-							<div className="flex items-center">
-								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#F5752D'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">10%</span></div>
-								<div className="text-white text-14px">Initial LP</div>
+							<div className="flex items-center" style={{}}>
+								<div className="relative flex-shrink-0 w-10 h-10 mr-3" style={{'background': '#642438', borderStyle: 'solid', borderWidth: '2px', borderRadius:'7px', borderColor:'#c93150'}}><span className="absolute block w-full text-center text-white transform -translate-y-1/2 top-1/2 text-12px">23%</span></div>
+								<div className="text-white text-14px">Staking/LP Rewards</div>
 							</div>
 						</div>
 						</div>
 					<div>
-						<img src={'/images/charts/allocations.svg'} width={`100%`} height={`100%`} className=" bg-black-50" />
+						<img src={'/images/charts/allocations_graph.png'} width={`100%`} height={`100%`} className=" bg-black-50" />
 					</div>
 				</div>
 				<div className="mt-12 mb-4 md:mb-10 md:mt-36 breaker"></div>
