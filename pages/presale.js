e3d1de86b759e15fc7196193bcac6a2957a4afa3
ILikeMelons <94649134+ILikeMelons@users.noreply.github.com>
2022-04-30 13:59:41 -0400

    new presale page

--- pages/presale.js
+++ pages/presale.js
@@ -1,22 +1,122 @@
+import { useEffect, useState } from "react";
+
+import {
+  VStack,
+  useDisclosure,
+  Button,
+  Text,
+  HStack,
+  Select,
+  Input,
+  Box
+} from "@chakra-ui/react";
+import { useWeb3React } from "@web3-react/core";
 import Layout from '../components/Layout'
 import Navbar from "../components/Navbar"
+import Web3connect from '../components/Modals/web3connect';
+import useLocalStorage from "../hooks/useLocalStorage";
+import { networkParams } from "../networks";
+import { connectors } from "../connectors";
 import { Parallax } from 'react-parallax';
-import { triangle_solid_white, cross_solid_yellow, triangle_outline_pink, circle_solid_white, square_outline_yellow } from '../public/shapes'
+import { getUserBalanceBNB, getUserBalanceBUSD } from "../utils/web3";
+import bitrivals from '../public/bitrivals_icon.svg' 
+
+
 const Presale = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const {
+        library,
+        chainId,
+        account,
+        activate,
+        deactivate,
+        active
+      } = useWeb3React();
+
+    const [signature, setSignature] = useState("");
+    const [error, setError] = useState("");
+    const [network, setNetwork] = useState(undefined);
+    const [message, setMessage] = useState("");
+    const [signedMessage, setSignedMessage] = useState("");
+    const [verified, setVerified] = useState();
+    const [wrongChainMessage, setWrongChainMessage] = useState("");
+    const [balance, setBalance] = useState(0);
+
+    const refreshState = () => {
+        window.localStorage.setItem("provider", undefined);
+        setNetwork("");
+        setWrongChainMessage("");
+        setMessage("");
+        setSignature("");
+        setVerified(undefined);
+      };
+    
+      const disconnect = () => {
+        refreshState();
+        deactivate();
+      };
+
+    useEffect(() => {
+        const provider = window.localStorage.getItem("provider");
+        console.log(provider);
+        console.log(chainId);
+        console.log(account);
+        if(chainId !== undefined){
+          setWrongChainMessage("")
+          if(chainId !== 56){
+            setWrongChainMessage(`Wrong network selected! ChainID:${chainId}`)
+          }
+        }
+        if (provider){
+           activate(connectors[provider]);
+          if (account!==undefined) {
+            getUserBalanceBUSD(account).then((bal)=> setBalance(bal)).catch(e=>console.log(e))
+           
+          }
+        }else{
+          disconnect();
+        } 
+       
+        
+      }, [account]);
+
+      
     return(
         <Layout>
-            <Navbar hideLinks={true}/>
+            {/* <div className="pt-10 ml-32 flex overflow-y-hidden">
+             <Image src={bitrivals.src} width={150} height={24} alt='logo'/>
+
+            </div> */}
             <div className='w-full relative h-screen bg-brand flex justify-center items-center'>
             
                 <div className='p-2 flex justify-center'>
-               
-                    <h1 className='text-pink font-morgan text-30px'>PRESALE COMING SOON!</h1>
+                  
+                {console.log(account)}
+                    {/*<h1 className='text-pink font-morgan text-30px'>PRESALE COMING SOON!</h1> */}
+                    {!active
+                    ? 
+                  
+                    <button onClick={onOpen} className='text-white border-white border-2 px-4 py-1 rounded-lg hover:text-pink hover:border-pink'>
+                        Connect Wallet
+                    </button> : 
+                      <div>
+                       <button onClick={disconnect} className='text-white border-white border-2 px-4 py-1 rounded-lg hover:text-pink hover:border-pink'>
+                    Disconnect
+                </button>
+                <p className="text-white pt-2">Your address : {account && account}</p>
+                <p className="text-white pt-2">Your balance : {balance.toFixed(2)} BUSD</p>
+                <p className="text-pink">{wrongChainMessage}</p>
+                      </div>
+                   
+                    }
+                    <Web3connect isOpen={isOpen} closeModal={onClose} />
                     <div>
 
                     </div>
                 </div>
-                
+               
             </div>
+           
         </Layout>
     );
 }
